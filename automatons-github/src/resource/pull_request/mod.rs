33e529a382c50f8d88a4322efe787ab06d862f56
Jan David <jd@devxbots.com>
2022-08-04 15:29:57 +0200

    Create resource for pull requests (#31)
    Pull requests are a feature of GitHub to merge two branches. They give
    users the opportunity to review and comment on each other's code, and
    perform automated tests before merging. A new resource has been created
    for them.

---
+++ automatons-github/src/resource/pull_request/mod.rs
@@ -0,0 +1,117 @@
+use std::fmt::{Display, Formatter};
+
+use url::Url;
+
+use crate::id;
+
+pub use self::branch::PullRequestBranch;
+
+mod branch;
+
+id!(
+    /// Pull request id
+    ///
+    /// The [`PullRequestId`] is a unique, numerical id that is used to interact with a pull request
+    /// through [GitHub's REST API](https://docs.github.com/en/rest).
+    PullRequestId
+);
+
+id!(
+    /// Pull request number
+    ///
+    /// Every [`PullRequest`] has a unique, human-readable, monotonically increasing number assigned
+    /// to it. This number identifies the pull request on GitHub's website.
+    PullRequestNumber
+);
+
+/// Pull request
+///
+/// Pull requests are a feature of GitHub to merge two branches. Users can create, review, and merge
+/// pull requests using GitHub's platform. Each pull request has a unique `id`, a human-readable
+/// `number`, and references to the two branches.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub struct PullRequest {
+    id: PullRequestId,
+    number: PullRequestNumber,
+    url: Url,
+    head: PullRequestBranch,
+    base: PullRequestBranch,
+}
+
+impl PullRequest {
+    /// Returns the pull request's id.
+    #[cfg_attr(feature = "tracing", tracing::instrument)]
+    pub fn id(&self) -> PullRequestId {
+        self.id
+    }
+
+    /// Returns the pull request's number.
+    #[cfg_attr(feature = "tracing", tracing::instrument)]
+    pub fn number(&self) -> PullRequestNumber {
+        self.number
+    }
+
+    /// Returns the API endpoint to query the pull request.
+    #[cfg_attr(feature = "tracing", tracing::instrument)]
+    pub fn url(&self) -> &Url {
+        &self.url
+    }
+
+    /// Returns the pull request's head branch
+    #[cfg_attr(feature = "tracing", tracing::instrument)]
+    pub fn head(&self) -> &PullRequestBranch {
+        &self.head
+    }
+
+    /// Returns the pull request's base branch
+    #[cfg_attr(feature = "tracing", tracing::instrument)]
+    pub fn base(&self) -> &PullRequestBranch {
+        &self.base
+    }
+}
+
+impl Display for PullRequest {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "#{}", self.number)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PullRequest;
+
+    #[test]
+    #[cfg(feature = "serde")]
+    fn trait_deserialize() {
+        let pr: PullRequest = serde_json::from_str(include_str!(
+            "../../../tests/fixtures/resource/pull_request.json"
+        ))
+        .unwrap();
+
+        assert_eq!(27, pr.number().get());
+    }
+
+    #[test]
+    #[cfg(feature = "serde")]
+    fn trait_display() {
+        let pr: PullRequest = serde_json::from_str(include_str!(
+            "../../../tests/fixtures/resource/pull_request.json"
+        ))
+        .unwrap();
+
+        assert_eq!("#27", pr.to_string());
+    }
+
+    #[test]
+    fn trait_send() {
+        fn assert_send<T: Send>() {}
+        assert_send::<PullRequest>();
+    }
+
+    #[test]
+    fn trait_sync() {
+        fn assert_sync<T: Sync>() {}
+        assert_sync::<PullRequest>();
+    }
+}
