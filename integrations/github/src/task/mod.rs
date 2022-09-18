205a341594914b6e3697642667a216707db685eb
Jan David <jd@devxbots.com>
2022-09-18 15:41:51 +0200

    Create task that creates a check run (#52)
    A new task has been created that creates a check run. The task consumes
    a set of input parameters, and adds the check run to the shared state.

--- integrations/github/src/task/mod.rs
+++ integrations/github/src/task/mod.rs
@@ -2,10 +2,12 @@
 //!
 //! The GitHub integration implements tasks that can be used to create automatons.
 
+pub use self::create_check_run::{CreateCheckRun, CreateCheckRunInput};
 pub use self::list_check_runs_for_check_suite::ListCheckRunsForCheckSuite;
 pub use self::list_check_runs_for_git_sha::ListCheckRunsForGitSha;
 pub use self::list_check_suites::ListCheckSuites;
 
+mod create_check_run;
 mod list_check_runs_for_check_suite;
 mod list_check_runs_for_git_sha;
 mod list_check_suites;
