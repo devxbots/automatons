6361389cebc0471264d6d7b0624c6dfd3c3f9f2d
Jan David <jd@devxbots.com>
2022-09-28 20:56:56 +0200

    Replace all usages of outputs with args (#63)
    The two tasks that create and update check runs respectively have been
    refactored to use the new args type exclusively and consistently.

--- integrations/github/src/task/mod.rs
+++ integrations/github/src/task/mod.rs
@@ -2,6 +2,10 @@
 //!
 //! The GitHub integration implements tasks that can be used to create automatons.
 
+use serde::Serialize;
+
+use crate::resource::{CheckRunOutputSummary, CheckRunOutputTitle};
+
 pub use self::create_check_run::{CreateCheckRun, CreateCheckRunArgs};
 pub use self::get_file::GetFile;
 pub use self::list_check_runs_for_check_suite::ListCheckRunsForCheckSuite;
@@ -15,3 +19,22 @@ mod list_check_runs_for_check_suite;
 mod list_check_runs_for_git_sha;
 mod list_check_suites;
 mod update_check_run;
+
+/// Input for check run output
+///
+/// Check runs can accept a variety of data in the `output` object, including a `title` and
+/// `summary` and can optionally provide descriptive details about the run.
+///
+/// https://docs.github.com/en/rest/checks/runs#update-a-check-run
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Serialize)]
+pub struct CheckRunOutputArgs {
+    /// The title of the check run output.
+    pub title: CheckRunOutputTitle,
+
+    /// The summary of the check run output.
+    pub summary: CheckRunOutputSummary,
+
+    /// The text with descriptive details about the check run.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text: Option<String>,
+}
